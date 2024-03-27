c7e4dccd23b0d190f246dd55860ad3a0249030e5
DanielBrisch <danielbrisch157@gmail.com>
2024-03-27 00:48:47 -0300

    finish

--- main.js
+++ main.js
@@ -1,52 +1,45 @@
-const puppeteer = require('puppeteer');
-const wifiControl = require('wifi-control');
+const puppetter = require('puppeteer');
+const wifi = require('node-wifi');
 
-(async () => {
-    wifiControl.init({
-        debug: true
-    });
+wifi.init({
+    iface: null
+});
 
-    const checkWifiConnection = async () => {
-        return new Promise((resolve, reject) => {
-            wifiControl.getIfaceState('wlan0', function (err, status) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(status.ssid === 'UNIMATER');
-                }
-            });
-        });
-    };
+function startMainJs() {
+    (async () => {
+        const browser = await puppetter.launch();
+        const page = await browser.newPage();
 
-    const isConnectedWifi = await checkWifiConnection();
-    if (!isConnectedWifi) {
-        console.log("Não conectado à rede UNIMATER. Aguardando conexão...");
-        return;
-    }
+        // Adicione aqui suas credenciais
+        var RA = ''
+        var password = ''
 
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        await page.goto('http://192.168.8.1:8002/index.php?zone=unimater&redirurl=http%3A%2F%2Fdetectportal.brave-http-only.com%2F');
 
-    await page.goto('http://192.168.8.1:8002/index.php?zone=unimater&redirurl=http%3A%2F%2Fdetectportal.brave-http-only.com%2F');
+        await page.type('#auth_user', RA);
 
-    const isConnectec = await page.evaluate(() => {
-        return document.body.textContent.trim() === 'You are connected';
-    });
+        await page.type('#auth_pass', password);
 
-    if (isConnectedWifi) {
-        console.log("Conectado. Não é necessário prosseguir.");
+        await page.click('#check');
+
+        await
+            await page.waitForSelector('#login');
+
+        await page.click('#login');
+
         await browser.close();
+
+    })();
+}
+
+wifi.getCurrentConnections((error, currentConnections) => {
+    if (error) {
+        startMainJs();
         return;
     }
+    currentConnections[0].bssid === 'UNIMATER' ? startMainJs() : console.log('Not Connected to UNIMATER');
+});
 
-    await page.type('#auth_user', '12218285');
-    await page.type('#auth_pass', 'Danimon159!');
-    await page.click('#check');
 
-    await page.waitForSelector('#login');
-    await page.click('#login');
-
-    await browser.close();
-})();
