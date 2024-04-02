31335b473cbaeb329f7d5e0a3f5cee5262499362
DanielBrisch <danielbrisch157@gmail.com>
2024-04-02 19:08:00 -0300

    finish

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless") 
+chrome_options.add_argument("--disable-gpu") 
 
 driver = webdriver.Chrome()
 
 driver.get('http://192.168.8.1:8002/index.php?zone=unimater&redirurl=http%3A%2F%2Fdetectportal.brave-http-only.com%2F')
 
-RA = ''
-password = ''
+time.sleep(2)
 
-driver.find_element_by_id("auth_user").send_keys(RA)
 
-driver.find_element_by_id("auth_pass").send_keys(password)
+# adicione o seu RA dentro da funcao send_keys
+driver.find_element(By.ID,"auth_user").send_keys('')
+# adicione sua senha dentro da funcao send_keys
+driver.find_element(By.ID,"auth_pass").send_keys('')
 
-driver.find_element_by_id("check").click()
+driver.find_element(By.ID,"check").click()
 
 time.sleep(2)
 
-driver.find_element_by_id("login").click()
+driver.find_element(By.ID,"login").click()
 
 # driver.quit()
